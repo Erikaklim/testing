1ab0d84181f081da5c33da9c9e4cfc513c8528d1
Erika <erikaklimait@gmail.com>
2024-03-13 17:56:05 +0200

    fix

--- src/test/java/TestTask4.java
+++ src/test/java/TestTask4.java
@@ -2,7 +2,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -141,3 +143,4 @@ public class TestTask4 {
     }
 
 }
+
